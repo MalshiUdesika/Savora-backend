d2f9012ccdacacf4cd07f741314b0e602dbe8a36
MalshiUdesika <malshigamage15@gmail.com>
2026-05-13 18:13:39 +0530

    install mongoose and connect with database ✏️

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+let mongoURI = "mongodb+srv://admin:1234@cluster0.ogiafsm.mongodb.net/?appName=Cluster0"
+
+mongoose.connect(mongoURI);
+
+mongoose.connect(mongoURI).then(
+    ()=>{
+        console.log("Connected to mongoDB")
+    }
+).catch(
+    ()=>{
+        console.log("Error connecting to mongoDB")
+    }
+)
 
 const app = express();
 
